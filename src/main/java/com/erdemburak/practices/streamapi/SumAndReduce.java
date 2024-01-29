13e6e224f064ba1ff4da89acd098fdf59ef1b0c5
erdemburak <burakerdem6@hotmail.com>
2024-01-29 15:55:18 +0300

    Stream - .reduce(Integer::sum).orElse(0)

--- src/main/java/com/erdemburak/practices/streamapi/SumAndReduce.java
+++ src/main/java/com/erdemburak/practices/streamapi/SumAndReduce.java
@@ -1,12 +1,14 @@
 package com.erdemburak.practices.streamapi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SumAndReduce {
     public static void main(String[] args) {
 
         List<Integer> num = new ArrayList<>(List.of(3,4,4,5,2,5,6,7,2,2));
+        //List<Integer> num = Arrays.asList(2,4,34,5,23,2,5,6,3,34,2,23,5,6,33,12);
 
         System.out.println(calculate2(num));
 
